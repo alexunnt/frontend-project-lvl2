c7e60486c7c00d5636aea2f56dfaac6b88086728
Aleksei Bulgakov <bulgakov@tutamail.com>
2020-03-05 17:54:40 +0700

    make gendiff

---
+++ src/bin/gendiff.js
@@ -0,0 +1,9 @@
+#!/usr/bin/env node
+import program from 'commander';
+
+program
+
+  .version('0.1.0', '-V, --version', 'output the current version')
+  .description('Compares two configuration files and shows a difference.');
+
+program.parse(process.argv);
