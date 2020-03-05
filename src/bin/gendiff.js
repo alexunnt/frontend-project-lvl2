b5b9a3d9a515d25fb98ba8dd76a3caf35813826e
Aleksei Bulgakov <bulgakov@tutamail.com>
2020-03-05 19:08:01 +0700

    add option and arguments

--- src/bin/gendiff.js
+++ src/bin/gendiff.js
@@ -3,7 +3,9 @@ import program from 'commander';
 
 program
 
-  .version('0.1.0', '-V, --version', 'output the current version')
-  .description('Compares two configuration files and shows a difference.');
+  .version('0.1.0', '-V, --version', 'output the version number')
+  .description('Compares two configuration files and shows a difference.')
+  .option('-f, --format [type]', 'output format')
+  .arguments('<firstConfig> <secondConfig>');
 
 program.parse(process.argv);
