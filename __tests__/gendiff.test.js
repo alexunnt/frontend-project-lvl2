2b1185e74b4a4677a46ff9cdc0763cce5044fb1e
Aleksei Bulgakov <bulgakov@tutamail.com>
2020-03-09 22:27:00 +0700

    add tests for json

---
+++ __tests__/gendiff.test.js
@@ -0,0 +1,11 @@
+import fs from 'fs';
+import genDiff from '../src';
+
+describe('Comparing two configuration files and showing a difference', () => {
+  it('json', () => {
+    const firstFile = '__tests__/__fixtures__/before.json';
+    const secondFile = '__tests__/__fixtures__/after.json';
+    const expected = fs.readFileSync(`${__dirname}/__fixtures__/result.txt`, 'utf-8').trim();
+    expect(genDiff(firstFile, secondFile)).toEqual(expected);
+  });
+});
