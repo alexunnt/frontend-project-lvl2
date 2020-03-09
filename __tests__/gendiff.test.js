79fd9a0d7dc8ba6f3557377dabad222000a58306
Aleksei Bulgakov <bulgakov@tutamail.com>
2020-03-09 22:44:09 +0700

    add tests for yml

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -8,4 +8,10 @@ describe('Comparing two configuration files and showing a difference', () => {
     const expected = fs.readFileSync(`${__dirname}/__fixtures__/result.txt`, 'utf-8').trim();
     expect(genDiff(firstFile, secondFile)).toEqual(expected);
   });
+  it('yaml', () => {
+    const firstFile = '__tests__/__fixtures__/before.yml';
+    const secondFile = '__tests__/__fixtures__/after.yml';
+    const expected = fs.readFileSync(`${__dirname}/__fixtures__/result.txt`, 'utf-8').trim();
+    expect(genDiff(firstFile, secondFile)).toEqual(expected);
+  });
 });
