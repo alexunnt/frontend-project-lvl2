aff3ed521595f5a047f422ca1a94698ca30cbf97
Aleksei Bulgakov <bulgakov@tutamail.com>
2020-03-10 01:55:31 +0700

    add test for ini

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -14,4 +14,11 @@ describe('Comparing two configuration files and showing a difference', () => {
     const expected = fs.readFileSync(`${__dirname}/__fixtures__/result.txt`, 'utf-8').trim();
     expect(genDiff(firstFile, secondFile)).toEqual(expected);
   });
+
+  it('ini', () => {
+    const firstFile = '__tests__/__fixtures__/before.ini';
+    const secondFile = '__tests__/__fixtures__/after.ini';
+    const expected = fs.readFileSync(`${__dirname}/__fixtures__/result.txt`, 'utf-8').trim();
+    expect(genDiff(firstFile, secondFile)).toEqual(expected);
+  });
 });
