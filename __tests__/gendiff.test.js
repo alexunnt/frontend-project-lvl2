fd312f52dcffba479547a0bc9786ee25c677bd18
Aleksei Bulgakov <bulgakov@tutamail.com>
2020-03-11 05:28:54 +0700

    add tests for tree

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -1,7 +1,7 @@
 import fs from 'fs';
 import genDiff from '../src';
 
-describe('Comparing two configuration files and showing a difference', () => {
+describe('Comparing two configuration files and showing a difference.', () => {
   it('json', () => {
     const firstFile = '__tests__/__fixtures__/before.json';
     const secondFile = '__tests__/__fixtures__/after.json';
@@ -19,6 +19,13 @@ describe('Comparing two configuration files and showing a difference', () => {
     const firstFile = '__tests__/__fixtures__/before.ini';
     const secondFile = '__tests__/__fixtures__/after.ini';
     const expected = fs.readFileSync(`${__dirname}/__fixtures__/result.txt`, 'utf-8').trim();
+    expect(genDiff(firstFile, secondFile)).toEqual(expected);
+  });
+
+  it('tree', () => {
+    const firstFile = '__tests__/__fixtures__/beforeTree.json';
+    const secondFile = '__tests__/__fixtures__/afterTree.json';
+    const expected = fs.readFileSync(`${__dirname}/__fixtures__/resultTree.txt`, 'utf-8').trim();
     expect(genDiff(firstFile, secondFile)).toEqual(expected);
   });
 });
