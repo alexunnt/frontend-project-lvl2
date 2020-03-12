fa450fc00bada9773fd57b563f30e0decc9dea9b
Aleksei Bulgakov <bulgakov@tutamail.com>
2020-03-13 01:11:25 +0700

    add json formatter

--- src/formatters/index.js
+++ src/formatters/index.js
@@ -1,9 +1,11 @@
 import getComplex from './complex.js';
 import getPlain from './plain.js';
+import getJson from './json.js';
 
 const outputType = {
   complex: getComplex,
   plain: getPlain,
+  json: getJson,
 };
 
 export default (data, format) => outputType[format](data);
