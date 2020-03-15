8c6031b4cf853011544f64d1e39f75f332672e6c
Aleksei Bulgakov <bulgakov@tutamail.com>
2020-03-15 08:53:53 +0700

    reduce => map

--- src/buildDiff.js
+++ src/buildDiff.js
@@ -3,54 +3,54 @@ import _ from 'lodash';
 const buildDifference = (firstObjectData, secondObjectData) => {
   const uniqueKeys = _.union(Object.keys(firstObjectData), Object.keys(secondObjectData));
 
-  return uniqueKeys.reduce((acc, item) => {
+  return uniqueKeys.map((item) => {
     const firstObjectItem = firstObjectData[item];
     const secondObjectItem = secondObjectData[item];
     if (_.has(firstObjectData, item) && _.has(secondObjectData, item)) {
       const checkingFirstObject = _.isObject(firstObjectItem);
       const checkingSecondObject = _.isObject(secondObjectItem);
       if (checkingFirstObject && checkingSecondObject) {
-        return [...acc, {
+        return {
           type: 'object',
           key: item,
           children: buildDifference(firstObjectItem, secondObjectItem),
-        }];
+        };
       }
       if (firstObjectItem === secondObjectItem) {
-        return [...acc, {
+        return {
           type: 'unchanged',
           key: item,
           value: firstObjectItem,
-        }];
+        };
       }
       if (checkingFirstObject || checkingSecondObject) {
-        return [...acc, {
+        return {
           type: 'changed',
           key: item,
           firstValue: firstObjectItem,
           secondValue: secondObjectItem,
-        }];
+        };
       }
-      return [...acc, {
+      return {
         type: 'changed',
         key: item,
         firstValue: firstObjectItem,
         secondValue: secondObjectItem,
-      }];
+      };
     }
     if (_.has(firstObjectData, item)) {
-      return [...acc, {
+      return {
         type: 'deleted',
         key: item,
         value: firstObjectItem,
-      }];
+      };
     }
-    return [...acc, {
+    return {
       type: 'added',
       key: item,
       value: secondObjectItem,
-    }];
-  }, []);
+    };
+  });
 };
 
 export default buildDifference;
