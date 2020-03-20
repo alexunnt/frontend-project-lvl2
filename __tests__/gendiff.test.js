f31bed5245c3c2aaa9545b6cddeafa3df7f2ea0b
Aleksei Bulgakov <bulgakov@tutamail.com>
2020-03-20 11:16:55 +0700

    edit tests and output format

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -1,48 +1,22 @@
 import fs from 'fs';
+import path from 'path';
 import genDiff from '../src';
 
-const pathToFixtures = '__tests__/__fixtures__/';
+const getFixturePath = (filename) => path.join(__dirname, '../__tests__', '__fixtures__', filename);
+const readFileSync = (filename) => fs.readFileSync(getFixturePath(filename), 'utf-8');
 
-describe('Comparing two configuration files and showing a difference.', () => {
-  it('json', () => {
-    const firstFile = `${pathToFixtures}before.json`;
-    const secondFile = `${pathToFixtures}after.json`;
-    const expected = fs.readFileSync(`${__dirname}/__fixtures__/result.txt`, 'utf-8').trim();
-    expect(genDiff(firstFile, secondFile)).toEqual(expected);
-  });
+const cases = [
+  ['json', 'complex', 'result.txt'],
+  ['ini', 'complex', 'result.txt'],
+  ['yml', 'complex', 'result.txt'],
+  ['json', 'plain', 'resultPlain.txt'],
+  ['json', 'json', 'resultJson.txt'],
+];
 
-  it('yaml', () => {
-    const firstFile = `${pathToFixtures}before.yml`;
-    const secondFile = `${pathToFixtures}after.yml`;
-    const expected = fs.readFileSync(`${__dirname}/__fixtures__/result.txt`, 'utf-8').trim();
-    expect(genDiff(firstFile, secondFile)).toEqual(expected);
-  });
-
-  it('ini', () => {
-    const firstFile = `${pathToFixtures}before.ini`;
-    const secondFile = `${pathToFixtures}after.ini`;
-    const expected = fs.readFileSync(`${__dirname}/__fixtures__/result.txt`, 'utf-8').trim();
-    expect(genDiff(firstFile, secondFile)).toEqual(expected);
-  });
-
-  it('tree', () => {
-    const firstFile = `${pathToFixtures}beforeTree.json`;
-    const secondFile = `${pathToFixtures}afterTree.json`;
-    const expected = fs.readFileSync(`${__dirname}/__fixtures__/resultTree.txt`, 'utf-8').trim();
-    expect(genDiff(firstFile, secondFile)).toEqual(expected);
-  });
-
-  it('plain output', () => {
-    const firstFile = `${pathToFixtures}before.json`;
-    const secondFile = `${pathToFixtures}after.json`;
-    const expected = fs.readFileSync(`${__dirname}/__fixtures__/resultPlain.txt`, 'utf-8').trim();
-    expect(genDiff(firstFile, secondFile, 'plain')).toEqual(expected);
-  });
-
-  it('json output', () => {
-    const firstFile = `${pathToFixtures}before.json`;
-    const secondFile = `${pathToFixtures}after.json`;
-    const expected = fs.readFileSync(`${__dirname}/__fixtures__/resultJson.txt`, 'utf-8').trim();
-    expect(genDiff(firstFile, secondFile, 'json')).toEqual(expected);
-  });
+test.each(cases)('Comparing two configuration files.', (extension, format, pathToResult) => {
+  const expectedResult = readFileSync(pathToResult);
+  const path1 = getFixturePath(`before.${extension}`);
+  const path2 = getFixturePath(`after.${extension}`);
+  const result = genDiff(path1, path2, format);
+  expect(result).toEqual(expectedResult);
 });
--- src/formatters/complex.js
+++ src/formatters/complex.js
@@ -1,6 +1,6 @@
 import _ from 'lodash';
 
-const indention = (value) => ' '.repeat(value * 2);
+const indention = (num) => ' '.repeat(num * 2);
 
 const stringify = (data, level) => {
   if (!_.isObject(data)) {
@@ -9,11 +9,11 @@ const stringify = (data, level) => {
   const keys = Object.keys(data);
   const gettingInner = keys.map((item) => {
     if (!_.isObject(item)) {
-      return `${indention(level + 2)}${item}: ${data[item]}`;
+      return `${indention(level + 5)}${item}: ${data[item]}`;
     }
-    return `${indention(level + 2)}${item}: ${stringify(item, level)}`;
+    return `${indention(level + 5)}${item}: ${stringify(item, level)}`;
   });
-  return _.flattenDeep(['{', gettingInner, `${indention(level)}  }`]).join('\n');
+  return _.flattenDeep(['{', gettingInner, `${indention(level + 2)}  }`]).join('\n');
 };
 
 const getOutput = (item, depth = 0) => {
@@ -22,18 +22,18 @@ const getOutput = (item, depth = 0) => {
   } = item;
   switch (type) {
     case 'object':
-      return [`${indention(depth + 1)}${key}: {`,
+      return [`  ${indention(depth + 2)}${key}: {`,
         ...children.map((node) => getOutput(node, depth + 1)),
-        `${indention(depth + 1)}}`];
+        `${indention(depth + 1)}  }`];
     case 'unchanged':
-      return `${indention(depth + 1)}${key}: ${stringify(value, depth)}`;
+      return `  ${indention(depth + 2)}${key}: ${stringify(value, depth)}`;
     case 'changed':
-      return [`${indention(depth)}- ${key}: ${stringify(firstValue, depth)}`,
-        `${indention(depth)}+ ${key}: ${stringify(secondValue, depth)}`];
+      return [`${indention(depth + 2)}- ${key}: ${stringify(firstValue, depth)}`,
+        `${indention(depth + 2)}+ ${key}: ${stringify(secondValue, depth)}`];
     case 'deleted':
-      return `${indention(depth)}- ${key}: ${stringify(value, depth)}`;
+      return `${indention(depth + 2)}- ${key}: ${stringify(value, depth)}`;
     case 'added':
-      return `${indention(depth)}+ ${key}: ${stringify(value, depth)}`;
+      return `${indention(depth + 2)}+ ${key}: ${stringify(value, depth)}`;
     default:
       throw new Error('Type error');
   }
