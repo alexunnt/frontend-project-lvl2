237c42811f76e82b77d1419e659704eee80b3a0d
Aleksei Bulgakov <bulgakov@tutamail.com>
2020-03-24 23:25:11 +0700

    improve getOutput

--- src/formatters/plain.js
+++ src/formatters/plain.js
@@ -11,18 +11,16 @@ const getOutput = (item, parent = '') => {
   const {
     type, key, firstValue, secondValue, children, value,
   } = item;
-  switch (type) {
-    case 'object':
-      return children.map((node) => getOutput(node, `${key}.`));
-    case 'changed':
-      return `Property '${parent}${key}' was updated. From ${strignify(firstValue)} to ${strignify(secondValue)}`;
-    case 'deleted':
-      return `Property '${parent}${key}' was removed`;
-    case 'added':
-      return `Property '${parent}${key}' was added with value: ${strignify(value)}`;
-    default:
-      return null;
-  }
+  const getStatus = {
+    object: () => children.map((node) => getOutput(node, `${key}.`)),
+    changed: () => `Property '${parent}${key}' was updated. From ${strignify(firstValue)} to ${strignify(secondValue)}`,
+    deleted: () => `Property '${parent}${key}' was removed`,
+    added: () => `Property '${parent}${key}' was added with value: ${strignify(value)}`,
+    unchanged: () => null,
+  };
+  return getStatus[type]();
 };
 
-export default (ast) => _.flattenDeep(ast.map((item) => getOutput(item))).filter((item) => item !== null).join('\n');
+export default (ast) => ast.map((item) => getOutput(item))
+  .filter((item) => item !== null)
+  .join('\n');
